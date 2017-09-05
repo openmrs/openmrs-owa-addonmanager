0cbf63b0a980f49d4d0d801944f5fda9fa069aac
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-09-05 11:52:54 +0300

    [Chore]Install testing dev dependencies
    [Chore]Add vscode folder to gitignore
    
    [Chore]Configure babel & jsdom
    
    [Chore]Write tests for APIHelper

--- app/js/helpers/apiHelper.js
+++ app/js/helpers/apiHelper.js
@@ -4,57 +4,58 @@ const applicationDistribution = location.href.split('/')[3];
 const apiBaseUrl = `/${applicationDistribution}/ws/rest/v1`; 
 
 export class ApiHelper {
-    constructor(requestLibrary) {
-        this.ALLOWED_TYPES = ['GET', 'DELETE', 'POST'];
-        this.requestLibrary = requestLibrary || (window.fetch ? window.fetch : fetchPolyfill);
-        this.mocked = !(requestLibrary == undefined);
-        this.requestOptions = {
-            credentials: 'include'
-        };
+  constructor(requestLibrary) {
+    this.ALLOWED_TYPES = ['GET', 'DELETE', 'POST'];
+    this.requestLibrary = requestLibrary || (window.fetch ? window.fetch : fetchPolyfill);
+    this.mocked = !(requestLibrary == undefined);
+    this.requestOptions = {
+      credentials: 'include'
+    };
+  }
+
+  build(requestUrl, requestType, requestData = {}) {
+    if (requestType && !this.ALLOWED_TYPES.includes(requestType)){
+      throw new Error('Invalid Request Type');
     }
 
-    build(requestUrl, requestType, requestData = {}) {
-        if (requestType && !this.ALLOWED_TYPES.includes(requestType)){
-            throw new Error('Invalid Request Type');
-        }
-        this.requestUrl = requestUrl;
-        let options = {};
-        const contentTypeHeader = 'application/json'
+    this.requestUrl = requestUrl;
+    let options = {};
+    const contentTypeHeader = 'application/json';
 
-        if (requestType != 'GET'){
-            options = {
-                method: requestType,
-                headers: {
-                    "Content-Type": contentTypeHeader
-                },
-                body: JSON.stringify(requestData)
-            };
-        }
-        this.requestOptions = options;
-        return this;
+    if (requestType != 'GET'){
+      options = {
+        method: requestType,
+        headers: {
+          "Content-Type": contentTypeHeader
+        },
+        body: JSON.stringify(requestData)
+      };
     }
+    this.requestOptions = options;
+    return this;
+  }
 
-    send() {
-        const request = this.requestLibrary;
-        const response = request(`${apiBaseUrl}${this.requestUrl}`, this.requestOptions)
-            .then((data)=>{
-                return this.mocked ? data : data.json();
-            })
-            .catch((error)=> {
-                return error;
-            });
-        return response;
-    }
+  send() {
+    const request = this.requestLibrary;
+    const response = request(`${apiBaseUrl}${this.requestUrl}`, this.requestOptions)
+      .then((data)=>{
+        return this.mocked ? data : data.json();
+      })
+      .catch((error)=> {
+        return error;
+      });
+    return response;
+  }
 
-    get(requestUrl) {
-        return this.build(requestUrl, 'GET').send();
-    }
+  get(requestUrl) {
+    return this.build(requestUrl, 'GET').send();
+  }
 
-    post(requestUrl, requestData) {
-        return this.build(requestUrl, 'POST', requestData).send();
-    }
+  post(requestUrl, requestData) {
+    return this.build(requestUrl, 'POST', requestData).send();
+  }
 
-    delete(requestUrl) {
-        return this.build(requestUrl, 'DELETE').send();
-    }
+  delete(requestUrl) {
+    return this.build(requestUrl, 'DELETE').send();
+  }
 }
