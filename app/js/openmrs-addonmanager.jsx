cf76ea15765553e8c8611f9e4e540fbd1043cdc7
wanjikum <wanjikumillicent5@gmail.com>
2017-09-09 09:14:22 +0300

    [chore] Add a changes to reflect codacy

--- app/js/openmrs-addonmanager.jsx
+++ app/js/openmrs-addonmanager.jsx
@@ -8,12 +8,11 @@
  */
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router'
+import {Router, Route, hashHistory} from 'react-router';
 
-import routes from './routes'
+import routes from './routes';
 
 render((
-         <Router history={hashHistory}>
-           {routes()}
-         </Router>
-       ), document.getElementById('app'));
+  <Router history={hashHistory}>
+    {routes()}
+  </Router>), document.getElementById('app'));
