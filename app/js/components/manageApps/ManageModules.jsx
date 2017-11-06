1b4a3bf609a88c78887a84686f838ce7b2ed5747
mkiterian <hmukite@gmail.com>
2017-11-06 10:05:13 +0300

    Add manage modules component

---
+++ app/js/components/manageApps/ManageModules.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ManageModules extends Component {
+  render() {
+    return <div>This is manage modules</div>;
+  }
+}
+
+export default ManageModules;
