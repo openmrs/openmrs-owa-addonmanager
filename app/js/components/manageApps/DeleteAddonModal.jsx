d41d667295bda53d6f4dc3a140fcd06b4848da93
Male Michael <michael.male@andela.com>
2017-11-24 11:59:48 +0300

    Implement deleting of installed modules (#69)

--- app/js/components/manageApps/DeleteAddonModal.jsx
+++ app/js/components/manageApps/DeleteAddonModal.jsx
@@ -12,27 +12,49 @@ import { Modal, ModalHeader, ModalTitle, ModalClose, ModalBody, ModalFooter} fro
 
 export default class DeleteModal extends Component {
   render() {
-    const { app, handleDelete, isOpen, hideModal } = this.props;
+    const { app, handleDelete, isOpen, hideModal, appUuid, affectedModules } = this.props;
     return (
-      <Modal isOpen={isOpen} onRequestHide={hideModal}>
-        <ModalHeader>
-          <ModalClose onClick={hideModal}/>
-          <ModalTitle>Confirm Delete</ModalTitle>
-        </ModalHeader>
-        <ModalBody>
-          <p>Are you sure you want to delete <strong>{app.name}</strong>?</p>
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-default" onClick={hideModal}>Cancel</button>
-          <button
-            type="button"
-            className="btn btn-danger"
-            data-dismiss="modal"
-            id="cancel-btn"
-            onClick={() => handleDelete(app.name)}
-          >Delete</button>
-        </ModalFooter>
-      </Modal> 
+      affectedModules ?
+        <Modal isOpen={isOpen} onRequestHide={hideModal}>
+          <ModalHeader>
+            <ModalClose onClick={hideModal}/>
+            <ModalTitle><span className="dependency-alert">Attention. Dependent Modules Detected!</span></ModalTitle>
+          </ModalHeader>
+          <ModalBody>
+            <p>The following modules will be shutdown if you decide to continue to delete <strong>{app.name}</strong>:</p>
+            <p> {affectedModules} </p>
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-default" onClick={hideModal}>Cancel</button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              data-dismiss="modal"
+              id="cancel-btn"
+              onClick={() => handleDelete(app.name, appUuid)}
+            >Delete</button>
+          </ModalFooter>
+        </Modal>
+        :
+        <Modal isOpen={isOpen} onRequestHide={hideModal}>
+          <ModalHeader>
+            <ModalClose onClick={hideModal}/>
+            <ModalTitle>Confirm Delete</ModalTitle>
+          </ModalHeader>
+          <ModalBody>
+            <p>Are you sure you want to delete <strong>{app.name}</strong>?</p>
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-default" onClick={hideModal}>Cancel</button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              data-dismiss="modal"
+              id="cancel-btn"
+              onClick={() => handleDelete(app.name, appUuid)}
+            >Delete</button>
+          </ModalFooter>
+        </Modal>
     );
   }
 }
@@ -41,5 +63,7 @@ DeleteModal.propTypes = {
   app: React.PropTypes.object.isRequired,
   handleDelete: React.PropTypes.func.isRequired,
   isOpen: React.PropTypes.bool.isRequired,
-  hideModal: React.PropTypes.func.isRequired
+  hideModal: React.PropTypes.func.isRequired,
+  appUuid: React.PropTypes.string.isRequired,
+  affectedModules: React.PropTypes.string.isRequired
 };
