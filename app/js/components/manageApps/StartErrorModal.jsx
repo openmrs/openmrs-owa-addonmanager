e7ed146044dd4d5a56cb93f0462fb4fa158681f8
mkiterian <hmukite@gmail.com>
2017-11-28 01:16:23 +0300

    Add component to display an error when a module fails to start (#74)

---
+++ app/js/components/manageApps/StartErrorModal.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Modal,
+  ModalHeader,
+  ModalTitle,
+  ModalClose,
+  ModalBody,
+  ModalFooter
+} from 'react-modal-bootstrap';
+
+export default class StartErrorModal extends Component {
+  render() {
+    const {
+      isOpen,
+      hideMsgModal,
+      message,
+      app,
+    } = this.props;
+    return (
+      <Modal isOpen={isOpen} onRequestHide={hideMsgModal}>
+        <ModalHeader>
+          <ModalClose onClick={hideMsgModal} />
+          <ModalTitle>
+            <span id="msg-modal-title">
+              <span className="glyphicon glyphicon-warning-sign" id="error-title-logo" />
+              {message}{app.name}
+            </span>
+          </ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <p>{app.startupErrorMessage}</p>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            className="btn btn-default btn-mark-ok"
+            onClick={hideMsgModal}>
+            Ok
+          </button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+StartErrorModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  app: PropTypes.object.isRequired,
+  message: PropTypes.string.isRequired,
+  hideMsgModal: PropTypes.func.isRequired,
+};
