97ce3b80660de2235e331370ff7e1276a5904cd9
Male Michael <michael.male@andela.com>
2018-02-16 13:35:20 +0300

    Update post data sent during OWA installation (#137)

--- app/js/components/manageApps/ManageApps.jsx
+++ app/js/components/manageApps/ManageApps.jsx
@@ -702,7 +702,6 @@ export default class ManageApps extends React.Component {
     if (addon && addon.appDetails.type === 'OWA') {
       this.requestUrl = '/owa/installapp';
       let urlObject = {
-        'fileName': addon.appDetails.name,
         'urlValue': addon.appDetails.versions[0].downloadUri,
       };
       const data = JSON.stringify(urlObject);
